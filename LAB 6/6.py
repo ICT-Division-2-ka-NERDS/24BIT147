6fe484d6c180c57a8ea710f7bf8795b22f8bb1b0
anoushkaaithal <anoushkaaithal@gmail.com>
2025-04-23 12:02:05 +0530

    Delete LAB 6 directory

--- LAB 6/6.py
+++
@@ -1,8 +0,0 @@
-def q6():
-    fahrenheit = [32, 68, 104, 212, 50]  # Example temperatures
-    celsius = []
-
-    for f in fahrenheit:
-        celsius.append((f - 32) * 5 / 9)
-
-    print(celsius)
--- LAB 6/9.py
+++
@@ -1,7 +0,0 @@
-def q9():
-    list1 = [1, 2, 3, 4, 5, 6]
-    list2 = [4, 5, 6, 7, 8, 9]
-
-    list3 = [num for num in list1 if num not in list2]
-
-    print(list3)
